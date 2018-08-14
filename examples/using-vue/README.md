7f1022488da8c0d62d3e422e06039e4cc5ed43d1
Tiago Marques <tiago.marques@virtual-identity.com>
2018-08-14 17:59:05 +0100

    Added vue-cli2 webpack project

---
+++ examples/using-vue/README.md
@@ -0,0 +1,9 @@
+# using-vue
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn
---
+++ examples/using-vue/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
