ba0c9b1992373c967e9bb8e68c92b74c2730caaa
Tiago Marques <tiago.marques@virtual-identity.com>
2018-08-29 13:40:38 +0100

    Add polyfills and update bioelement

--- projectConfig.js
+++ projectConfig.js
@@ -10,12 +10,32 @@ module.exports = {
 			'enquire.js': 'dist/enquire.js',
 			'jquery-debouncedwidth': 'js/jquery.debouncedwidth.js',
 			'normalize.css': 'normalize.css',
-
-		}
+			'@webcomponents/webcomponentsjs': [
+				'custom-elements-es5-adapter.js',
+				'webcomponents-loader.js',
+				'webcomponents-ce.js',
+				'webcomponents-hi-ce.js',
+				'webcomponents-hi-sd-ce.js',
+				'webcomponents-hi-sd.js',
+				'webcomponents-hi.js',
+				'webcomponents-lite.js',
+				'webcomponents-sd-ce.js',
+				'webcomponents-sd.js',
+			],
+		},
 	},
 	uglify: {
 		ignoreList: [
-			'resources/js/vendor/custom-elements-es5-adapter.js'
-		]
-	}
+			'resources/js/vendor/custom-elements-es5-adaptor.js',
+			'resources/js/vendor/webcomponents-loader.js',
+			'resources/js/vendor/webcomponents-ce.js',
+			'resources/js/vendor/webcomponents-hi-ce.js',
+			'resources/js/vendor/webcomponents-hi-sd-ce.js',
+			'resources/js/vendor/webcomponents-hi-sd.js',
+			'resources/js/vendor/webcomponents-hi.js',
+			'resources/js/vendor/webcomponents-lite.js',
+			'resources/js/vendor/webcomponents-sd-ce.js',
+			'resources/js/vendor/webcomponents-sd.js',
+		],
+	},
 };
